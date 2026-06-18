db7136e3b40c7cc5b8e766354396925d83374e5e
Daniel Chernov <chernov.dani@gmail.com>
2026-06-18 09:55:15 +0300

    made a better struct for render.js and made the CreateCommentInputRow function

--- scripts/render.js
+++ scripts/render.js
@@ -1,6 +1,40 @@
-import Tweeter from "./model"
 
+const Renderer = function () {
 
-function renderPosts(posts){
+    const createDeletePostButton = function(postId) {
 
+    }
+
+    const createCommentElement = function(comment) {
+    }
+
+    const createCommentsSection = function(comments) {
+    }
+
+    const createCommentInputRow = function() {
+        const commentRow = document.createElement("div")
+        const inputComment = document.createElement("input")
+        const btnComment = document.createElement("button")
+        inputComment.placeholder = "Got something to say?"
+        inputComment.classList.add("comment-input")
+
+        btnComment.textContent = "Comment"
+        btnComment.classList.add("comment-button")
+
+        commentRow.append(inputComment, btnComment)
+        return commentRow
+    }
+
+    const createPostElement = function(post) {
+    }
+
+    const renderPosts = function(posts) {
+        const postContainer = document.querySelector(".posts-container")
+    }
+
+    return {
+        renderPosts
+    }
 }
+
+export default Renderer
